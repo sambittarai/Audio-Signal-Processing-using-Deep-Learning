228d08ffd4e4eb0165ee3dae07ab6164f4c0913b
Sambit Tarai <sambitarai17@gmail.com>
2020-11-24 12:45:59 +0530

    Data_Loading updated

--- Data_Loading.py
+++ Data_Loading.py
@@ -7,8 +7,9 @@ from scipy.io import wavfile
 from python_speech_features import mfcc, logfbank
 import librosa
 
-def plot_signal(signals):
-	fig, axes = plt.subplots(nrows=2, nclos=5, sharex=False, sharey=True, figsize=(20,5))
+
+def plot_signals(signals):
+	fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False, sharey=True, figsize=(20,5))
 	fig.suptitle('Time Series', size=16)
 	i = 0
 	for x in range(2):
@@ -16,7 +17,7 @@ def plot_signal(signals):
 			axes[x,y].set_title(list(signals.keys())[i])
 			axes[x,y].plot(list(signals.values())[i])
 			axes[x,y].get_xaxis().set_visible(False)
-			axes.get_yaxis().set_visible(False)
+			axes[x,y].get_yaxis().set_visible(False)
 			i += 1
 
 def plot_fft(fft):
@@ -57,7 +58,26 @@ def plot_mfccs(mfccs):
 			axes[x,y].get_yaxis().set_visible(False)
 			i += 1
 
+def calc_fft(signal, rate):
+	n = len(signal)
+	freq = np.fft.rfftfreq(n, d=1/rate)
+	Y = abs(np.fft.rfft(signal)/n)
+	return (Y, freq)
+ 
+def envelope(signal, rate, threshold):
+	#Get rid of the low magnitude signal from the signal
+	mask = []
+	y = pd.Series(signal).apply(np.abs)
+	y_mean = y.rolling(window=int(rate/10), min_periods=1, center=True).mean()
+	for mean in y_mean:
+		if mean > threshold:
+			mask.append(True)
+		else:
+			mask.append(False)
+	return mask
 
+
+# Loading the data
 df = pd.read_csv("/content/drive/MyDrive/Audio Signal Processing/Data/instruments.csv")
 df = df.drop(columns='Unnamed: 0')
 df.set_index('Index', inplace=True)
@@ -75,3 +95,41 @@ ax.pie(class_dist, labels = class_dist.index, autopct='%1.1f%%', shadow=False, s
 ax.axis('equal')
 plt.show()
 df.reset_index(inplace=True)
+
+#Visualizing the data
+signals = {}
+fft = {}
+fbank = {}
+mfccs = {}
+
+for c in classes:
+	wav_file = df[df.Label == c].iloc[0,0]
+	signal, rate = librosa.load(wav_file, sr=44100)
+	mask = envelope(signal, rate, 5e-4)
+	signal = signal[mask]
+	signals[c] = signal
+	fft[c] = calc_fft(signal, rate)
+	bank = logfbank(signal[:rate], rate, nfilt=26, nfft=1103).T
+	fbank[c] = bank
+	mel = mfcc(signal[:rate], rate, numcep=13, nfilt=26, nfft=1103).T
+	mfccs[c] = mel
+
+plot_signals(signals)
+plt.show()
+plot_fft(fft)
+plt.show()
+plot_fbank(fbank)
+plt.show()
+plot_mfccs(mfccs)
+plt.show()
+
+#Downsampling the audio files that we will be using for modelling
+clean_data_path = '/content/drive/MyDrive/Audio Signal Processing/Clean Data'
+for f in tqdm(df.Index):
+	signal, rate = librosa.load(f, sr=16000)#downsample to 16000
+	mask = envelope(signal, rate, 5e-4)
+	path = f.split('/')[-2:]
+	if os.path.exists(os.path.join(clean_data_path, path[0])) == False:
+		os.mkdir(os.path.join(clean_data_path, path[0]))
+	wavfile.write(filename = os.path.join(clean_data_path, path[0], path[1]),
+	 rate=rate, data=signal[mask])

afd56b01969422405ddc86eff1dfc2def8dadc8b
Sambit Tarai <sambitarai17@gmail.com>
2020-11-26 18:50:16 +0530

    config file is added

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ from sklearn.utils.class_weight import compute_class_weight
 from tqdm import tqdm
 from python_speech_features import mfcc
 from cfg import Config
+from model_architecture import get_conv_model, get_recurrent_model
 
 def check_data():
 	if os.path.isfile(config.p_path):
@@ -47,40 +48,6 @@ def build_rand_feat():
 	y = to_categorical(y, num_classes=10)
 
 	return X, y
-
-def get_conv_model():
-	model = Sequential()
-	model.add(Conv2D(16, (3, 3), activation='relu', strides=(1,1), padding='same', input_shape=input_shape))
-	model.add(Conv2D(32, (3, 3), activation='relu', strides=(1,1), padding='same'))
-	model.add(Conv2D(64, (3, 3), activation='relu', strides=(1,1), padding='same'))
-	model.add(Conv2D(128, (3, 3), activation='relu', strides=(1,1), padding='same'))
-	model.add(MaxPool2D((2, 2)))
-	model.add(Dropout(0.5))
-	model.add(Flatten())
-	model.add(Dense(128, activation='relu'))
-	model.add(Dense(64, activation='relu'))
-	model.add(Dense(10, activation='softmax'))
-	model.summary()
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
-
-	return model
-
-def get_recurrent_model():
-	#Shape of data for LSTM is (n, time, feature)
-	model = Sequential()
-	model.add(LSTM(128, return_sequences=True, input_shape=input_shape))
-	model.add(LSTM(128, return_sequences=True))
-	model.add(Dropout(0.5))
-	model.add(TimeDistributed(Dense(64, activation='relu')))
-	model.add(TimeDistributed(Dense(32, activation='relu')))
-	model.add(TimeDistributed(Dense(16, activation='relu')))
-	model.add(TimeDistributed(Dense(8, activation='relu')))
-	model.add(Flatten())
-	model.add(Dense(10, activation='softmax'))
-	model.summary()
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
-
-	return model
 
 
 df = pd.read_csv("/content/drive/MyDrive/Audio Signal Processing/Clean Data/instruments_clean.csv")
